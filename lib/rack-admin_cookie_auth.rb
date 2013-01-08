9e45a97606663ebcf73c8e006bd0f68eafb63fbf
Rémy Coutable <remy@jilion.com>
2013-01-08 21:08:12 +0100

    Remove useless files and update CHANGELOG

--- lib/rack-admin_cookie_auth.rb
+++
@@ -1,7 +0,0 @@
-require "rack-admin_cookie_auth/version"
-
-module Rack
-  module AdminCookieAuth
-    # Your code goes here...
-  end
-end
--- lib/rack-admin_cookie_auth/version.rb
+++
@@ -1,5 +0,0 @@
-module Rack
-  module AdminCookieAuth
-    VERSION = "0.0.1"
-  end
-end
